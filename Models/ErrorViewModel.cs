83943cb5d51d0a7f804c20cc380550492e327a64
WesTer <romanvs0602@gmail.com>
2024-04-01 23:11:23 +0300

    rename branch

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CourseWork.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
