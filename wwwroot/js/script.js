d645fb154a1bbbc5c4544f6955322946f0f7cf68
Dexteroq <redixtu.sxrs@gmail.com>
2024-05-22 16:16:06 +0300

    up

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -2,36 +2,60 @@
 
 let profile = document.querySelector('.header .flex .profile');
 
-document.querySelector('#user-btn').onclick = () =>{
-   profile.classList.toggle('active');
-   search.classList.remove('active');
+document.querySelector('#user-btn').onclick = () => {
+    profile.classList.toggle('active');
+    search.classList.remove('active');
 }
 
 let search = document.querySelector('.header .flex .search-form');
 
-document.querySelector('#search-btn').onclick = () =>{
-   search.classList.toggle('active');
-   profile.classList.remove('active');
+document.querySelector('#search-btn').onclick = () => {
+    search.classList.toggle('active');
+    profile.classList.remove('active');
 }
 
 let sideBar = document.querySelector('.side-bar');
 
-document.querySelector('#menu-btn').onclick = () =>{
-   sideBar.classList.toggle('active');
-   body.classList.toggle('active');
+document.querySelector('#menu-btn').onclick = () => {
+    sideBar.classList.toggle('active');
+    body.classList.toggle('active');
 }
 
-document.querySelector('#close-btn').onclick = () =>{
-   sideBar.classList.remove('active');
-   body.classList.remove('active');
+document.querySelector('#close-btn').onclick = () => {
+    sideBar.classList.remove('active');
+    body.classList.remove('active');
 }
 
-window.onscroll = () =>{
-   profile.classList.remove('active');
-   search.classList.remove('active');
+window.onscroll = () => {
+    profile.classList.remove('active');
+    search.classList.remove('active');
 
-   if(window.innerWidth < 1200){
-      sideBar.classList.remove('active');
-      body.classList.remove('active');
-   }
+    if (window.innerWidth < 1200) {
+        sideBar.classList.remove('active');
+        body.classList.remove('active');
+    }
+}
+
+
+
+function compileCode() {
+    var code = document.getElementById('code-input').value;
+    var output = document.getElementById('output');
+
+    // »спользуем fetch дл€ отправки кода на сервер дл€ компил€ции
+    fetch('/compile', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ code: code }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            // ѕолучаем результат от сервера и отображаем его
+            output.innerText = data.result;
+        })
+        .catch(error => {
+            output.innerText = "ќшибка компил€ции: " + error.message;
+        });
 }
