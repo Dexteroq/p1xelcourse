b9bdc4cb462f7476f797bcf992c72997ac7742b9
Dexteroq <redixtu.sxrs@gmail.com>
2024-05-25 12:51:09 +0300

    contact us implementation

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,11 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace CourseWork.Controllers
 {
     public class ContactController : Controller
     {
+        // Метод для отображения страницы контактов
         public IActionResult Index()
         {
+            return View("Contact");
+        }
+
+        // Метод для обработки POST-запроса с формы
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(string name, string email, string number, string msg)
+        {
+            if (ModelState.IsValid)
+            {
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("harrisonnn2023@gmail.com"));  // Ваша почта
+                message.From = new MailAddress(email);  // Почта отправителя
+                message.Subject = "Новое сообщение с контактной формы";
+                message.Body = $"Name: {name}\nEmail: {email}\nNumber: {number}\nMessage: {msg}";
+                message.IsBodyHtml = false;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "harrisonnn2023@gmail.com",  // Ваша почта
+                        Password = "cbye kufu vqaa nvpz"  // Ваш пароль
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                }
+
+                ViewBag.Message = "Сообщение отправлено!";
+                return RedirectToAction("Index", "Home");
+            }
+
             return View("Contact");
         }
     }
--- obj/Debug/net8.0/CourseWork.AssemblyInfo.cs
+++ obj/Debug/net8.0/CourseWork.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36cc00c766f793c8a135566e8d9dc1ffd8ce401c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfa7f173b3134c489542ef2deba3ba68a3c6ee4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
