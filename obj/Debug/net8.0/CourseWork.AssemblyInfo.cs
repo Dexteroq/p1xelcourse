0eac357cd78a2f45e9979c45c3c9d380170a7fb7
Dexteroq <redixtu.sxrs@gmail.com>
2024-05-29 10:30:06 +0300

    update module window 29.05

--- obj/Debug/net8.0/CourseWork.AssemblyInfo.cs
+++ obj/Debug/net8.0/CourseWork.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9bdc4cb462f7476f797bcf992c72997ac7742b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9b9db9e4476eef7772bdccc25af0a19bb6fb282")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
