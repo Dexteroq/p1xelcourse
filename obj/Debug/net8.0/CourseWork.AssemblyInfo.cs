cba216b216fb02fb5b08b474da481a72b72d1a2d
Dexteroq <redixtu.sxrs@gmail.com>
2024-06-06 17:48:36 +0300

    fix06

--- obj/Debug/net8.0/CourseWork.AssemblyInfo.cs
+++ obj/Debug/net8.0/CourseWork.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dff0bd7e35707b1386d876746c863819e020c9dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+795a3a990a30271f862032cd5c6c51238bdb50b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
