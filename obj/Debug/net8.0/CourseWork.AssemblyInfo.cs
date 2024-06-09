ce63764b8d67df6022ff33ca0ad77c8409825263
WesTer <romanvs0602@gmail.com>
2024-06-09 17:15:38 +0300

    Исправил игру, поменял чуть вью. Сохранение перед созданием коментариев

--- obj/Debug/net8.0/CourseWork.AssemblyInfo.cs
+++ obj/Debug/net8.0/CourseWork.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7059f0def8c1d3ec2d4a40e8377202040c99bfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b35602c85a61a6b8868e74e0bb1dbe10c47fdd78")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
