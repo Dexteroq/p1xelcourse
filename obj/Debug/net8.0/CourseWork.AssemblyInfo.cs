b35602c85a61a6b8868e74e0bb1dbe10c47fdd78
Dexteroq <redixtu.sxrs@gmail.com>
2024-06-09 13:46:10 +0300

    add snake game to TopCategories

--- obj/Debug/net8.0/CourseWork.AssemblyInfo.cs
+++ obj/Debug/net8.0/CourseWork.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5322eb3590d05e32c0535bab0568d6985efc8eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7059f0def8c1d3ec2d4a40e8377202040c99bfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
